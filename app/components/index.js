9fe3486c8a6e9be76a483df851f1e2250dc5e543
Dasajton <david.sajitz.94@gmail.com>
2024-08-19 18:07:26 +0200

    update import statements

--- app/components/index.js
+++ app/components/index.js
@@ -2,6 +2,7 @@ export { default as Ninja } from "./Ninja";
 export { default as Level } from "./Level";
 export { default as Lights } from "./Lights";
 export { default as Scroll } from "./Scroll";
+export { default as UICollectedItems } from "./UICollectedItems";
 
 export { default as animationSet } from "./animationsSet";
 export { default as keyboardMap } from "./keyboardControlls";
