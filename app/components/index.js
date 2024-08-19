89b21180d9f1372212ab4a8b5115b06338aee0ad
Dasajton <david.sajitz.94@gmail.com>
2024-08-19 11:06:32 +0200

    imports cleanup with index.js

---
+++ app/components/index.js
@@ -0,0 +1,7 @@
+export { default as Ninja } from "./Ninja";
+export { default as Level } from "./Level";
+export { default as Lights } from "./Lights";
+export { default as Scroll } from "./Scroll";
+
+export { default as animationSet } from "./animationsSet";
+export { default as keyboardMap } from "./keyboardControlls";
--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,17 @@
 "use client";
 import { KeyboardControls, OrbitControls, Stats } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import Lights from "./components/Lights";
-import Level from "./components/Level";
 import { Physics } from "@react-three/rapier";
 import { Suspense } from "react";
 import Ecctrl, { EcctrlAnimation } from "ecctrl";
-import keyboardMap from "./components/keyboardControlls";
-import Ninja from "./components/Ninja";
-import animationSet from "./components/animationsSet";
-import Scroll from "./components/Scroll";
+import {
+  Ninja,
+  Level,
+  Scroll,
+  Lights,
+  keyboardMap,
+  animationSet,
+} from "./components";
 
 const characterURL = "./ninja.glb";
 
