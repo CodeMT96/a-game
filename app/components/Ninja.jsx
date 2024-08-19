a7082499069278c48f6838920fef16f3f9bb8056
Dasajton <david.sajitz.94@gmail.com>
2024-08-19 18:06:45 +0200

    playerPosition as provided context

--- app/components/Ninja.jsx
+++ app/components/Ninja.jsx
@@ -2,10 +2,12 @@ import { useLoader } from "@react-three/fiber";
 import { RigidBody } from "@react-three/rapier";
 import { useRef } from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { useAppContext } from "../AppContext";
 
 export default function Ninja() {
   const gltf = useLoader(GLTFLoader, "./ninja.glb");
   const playerRef = useRef(gltf);
+  const { playerPosition, setPlayerPosition } = useAppContext();
   console.log(RigidBody);
 
   gltf.scene.traverse((object) => {
@@ -15,5 +17,5 @@ export default function Ninja() {
     }
   });
 
-  return <primitive position={[0, -0.9, 0]} object={gltf.scene} />;
+  return <primitive position={playerPosition} object={gltf.scene} />;
 }
