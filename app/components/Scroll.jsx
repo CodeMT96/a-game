508fbadb58aed2ca1abf529ab50529f7ae0661dd
Dasajton <david.sajitz.94@gmail.com>
2024-08-19 18:08:03 +0200

    (unfinished) logic for items to be looted

--- app/components/Scroll.jsx
+++ app/components/Scroll.jsx
@@ -1,10 +1,19 @@
-import { useRef } from "react";
-import { useLoader } from "@react-three/fiber";
+import { useRef, useState, useEffect, useMemo } from "react";
+import { useLoader, useFrame } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Raycaster } from "three";
+import { useAppContext } from "../AppContext";
 
 export default function Scroll({ position = [0, -0.5, 0] }) {
   const gltf = useLoader(GLTFLoader, "./scroll.glb");
-  const scrollRef = useRef(gltf);
+  const scene = useMemo(() => gltf.scene.clone(), [gltf]);
+  const scrollRef = useRef();
+  const [visible, setVisible] = useState(true);
+
+  const { itemsCollected, setItemsCollected } = useAppContext();
+
+  const raycaster = new Raycaster();
+  const pointer = useRef({ x: 0, y: 0 });
 
   gltf.scene.traverse((object) => {
     if (object.isMesh) {
@@ -13,12 +22,46 @@ export default function Scroll({ position = [0, -0.5, 0] }) {
     }
   });
 
+  const handlePointerMove = (event) => {
+    pointer.current.x = (event.clientX / window.innerWidth) * 2 - 1;
+    pointer.current.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  };
+
+  const handlePointerClick = () => {
+    if (raycaster.intersectObject(scrollRef.current).length > 0) {
+      setItemsCollected((prev) => prev + 1);
+      setVisible(false);
+    }
+  };
+
+  useFrame(({ camera }) => {
+    if (visible) {
+      raycaster.setFromCamera(pointer.current, camera);
+      const intersects = raycaster.intersectObjects(scene.children, true);
+      if (intersects.length > 0 && intersects[0].object === scrollRef.current) {
+        setVisible(false);
+      }
+    }
+  });
+
+  useEffect(() => {
+    window.addEventListener("mousemove", handlePointerMove);
+    window.addEventListener("click", handlePointerClick);
+
+    return () => {
+      window.removeEventListener("mousemove", handlePointerMove);
+      window.removeEventListener("click", handlePointerClick);
+    };
+  }, [handlePointerClick]);
+
   return (
-    <primitive
-      scale={0.1}
-      position={position}
-      object={gltf.scene}
-      ref={scrollRef}
-    />
+    visible && (
+      <primitive
+        scale={0.1}
+        position={position}
+        object={scene}
+        ref={scrollRef}
+      />
+    )
   );
 }
