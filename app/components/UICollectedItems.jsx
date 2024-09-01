0d0b2992931d9b14ca5c0c7d02da5243f81224ac
Dasajton <david.sajitz.94@gmail.com>
2024-09-01 18:40:46 +0200

    uiCollected bnackgroundimage now works

--- app/components/UICollectedItems.jsx
+++ app/components/UICollectedItems.jsx
@@ -2,5 +2,10 @@ import { useAppContext } from "../AppContext";
 
 export default function UICollectedItems() {
   const { itemsCollected } = useAppContext();
-  return <div className="uiCollected">{itemsCollected}</div>;
+  return (
+    <div className="uiCollected">
+      <img src="Ui.png" className="uiBG" />
+      {itemsCollected}
+    </div>
+  );
 }
